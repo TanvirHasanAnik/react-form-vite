118b535630d770f1dbb1312926f27d66870645e6
Anik <tanvir.h.anik@gmail.com>
2025-05-09 19:43:27 +0600

    added vivakits components

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import {z} from 'zod'
 import {useForm} from 'react-hook-form'
 import {DevTool} from "@hookform/devtools"
 import {zodResolver} from '@hookform/resolvers/zod';
-import { VKGroup,VKInput } from "@vivakits/react-components";
+import { VKButton,VKCheckbox,VKInput } from "@vivakits/react-components";
 
 
 
 const formSchema = z.object({
   username: z.string().nonempty("Username is required").min(3,"Username must be atleast 3 characters long").max(15, "username can't be more than 15 characters"),
   password: z.string().nonempty("Password is required").min(6,"password must contain atleast 6 characters").max(25,"password must not exceed 25 characters"),
-  age: z.number({invalid_type_error: "Age is required",}).gt(18).lt(150),
+  age: z.number({invalid_type_error: "Age is required",}).gt(18,"Must be adult").lt(150,"Give a realistic age"),
   email: z.string().nonempty("Email is required").email("invalid email"),
   isStudent: z.boolean(),
-  gender: z.enum(["male","female","others"],{required_error: "Please select gender"}),
+  gender: z.preprocess(val => val === '' ? undefined : val, z.enum(["male", "female", "others"], { required_error: "Please select gender" })) as z.ZodType<"male" | "female" | "others">,
   date: z.string().nonempty("Please provide a date"),
   time: z.string().nonempty("Please provide a time"),
 })
@@ -45,41 +45,20 @@ function App() {
    console.log('rendered')
    console.log(errors)
   return (
-    <div className="App bg-blue-100">
+    <div className="App w-full">
       <header className="App-header">
       </header>
       <main>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <VKGroup color="none" noSeparator border="none">
-      <VKInput variant="outline" label="Primary" color="primary" />
-      <VKInput variant="outline" label="Secondary" color="secondary" />
-      <VKInput variant="outline" label="Success" color="success" />
-      <VKInput variant="outline" label="Warning" color="warning" />
-      <VKInput variant="outline" label="Danger" color="danger" />
-      <VKInput variant="outline" label="Light" color="light" />
-    </VKGroup>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" {...register("username")}/>
-          {errors.username && <p style={{ color: 'red' }}>{errors.username.message}</p>}
-
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" {...register("password")}/>
-          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
-
-          <label htmlFor="age">Age</label>
-          <input type="number" id="age" {...register("age",{valueAsNumber: true})}/>
-          {errors.age && <p style={{ color: 'red' }}>{errors.age.message}</p>}
-
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" {...register("email")}/>
-          {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
-
-          <label htmlFor="isStudent">Student</label>
-          <input type="checkbox" id="isStudent" {...register("isStudent")}/>
+          <VKInput type='text' id='username' variant="outline" hasError={errors.username} errorMessage={errors?.username?.message} {...register("username")}  rounded="xl" label="Username"/>
+          <VKInput type="password" id="password" variant="outline" hasError={errors.password} errorMessage={errors?.password?.message} {...register("password")} rounded="xl" label="Password"/>
+          <VKInput label="age" type="number" id="age"  hasError={errors.age} errorMessage={errors?.age?.message} {...register("age",{valueAsNumber: true})} rounded="xl" variant="outline"/>
+          <VKInput label="email" type="email" id="email"  hasError={errors.email} errorMessage={errors?.email?.message} {...register("email")} rounded="xl" variant="outline"/>
+          <VKCheckbox rounded="full" id="isStudent" {...register("isStudent")}>Student</VKCheckbox>
           {errors.isStudent && <p style={{ color: 'red' }}>{errors.isStudent.message}</p>}
 
           <label htmlFor="gender">Gender</label>
-          <select id="gender" {...register("gender")}>
+          <select id="gender" {...register("gender")} ge>
             <option value="" disabled selected hidden>Select gender</option>
             <option value = "male">Male</option>
             <option value = "female">Female</option>
@@ -95,7 +74,9 @@ function App() {
           <input type="time" id="time" {...register("time")}/>
           {errors.time && <p style={{ color: 'red' }}>{errors.time.message}</p>}
 
-          <button type='submit'>Submit</button>
+          <VKButton type='submit' size="md" rounded="md">
+            Medium
+          </VKButton>
         </form>
         <DevTool control={control}/>
       </main>
